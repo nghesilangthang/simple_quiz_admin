1e69db79d16ad8803f248e9cfcd7922b5f2c6941
HanhThong <huynhhanhthong@gmail.com>
2015-08-06 19:57:34 +0700

    update model and migration

---
+++ database/migrations/2015_08_06_121917_create_questions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('question');
+            $table->text('option_1');
+            $table->text('option_2');
+            $table->text('option_3');
+            $table->text('option_4');
+            $table->text('answer');
+            $table->integer('user_create')->unsigned();
+            $table->foreign('user_create')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('questions');
+    }
+}
